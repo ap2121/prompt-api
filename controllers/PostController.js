d0c67f301da58088e64d5bc45c0d63786b0c0157
Arthur Pate <arthurpate42@gmail.com>
2023-03-24 23:13:28 -0700

    user can create post with caption and image prompt, caption prompt now just requires topic, photo url changed and stored using cloudinary api, urls generated by dall-e only last 2 hours, needed to implement cloudinary to fix problem

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const { Configuration, OpenAIApi } = require('openai')
 const {Post, sequelize} = require('../models')
+const cloudinary = require('cloudinary').v2
 
 const OPEN_AI_API_KEY = process.env.OPEN_AI_API_KEY
 const OPEN_AI_ORG_KEY = process.env.OPEN_AI_ORG_KEY
@@ -9,6 +10,12 @@ const configuration = new Configuration({
     organization: OPEN_AI_ORG_KEY
     
 })
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const openAi = new OpenAIApi(configuration)
 
@@ -26,7 +33,8 @@ const NewPost = async (req, res) => {
         const aiCapRes = await openAi.createCompletion({
             model: "text-davinci-003",
             prompt: `Write a very short instagram type caption about ${capPrompt}`,
-            temperature: 0.5
+            temperature: 0.5,
+            max_tokens: 150
 
 
         })
@@ -35,15 +43,17 @@ const NewPost = async (req, res) => {
         const photo = aiImgRes.data.data[0].url
         const caption = aiCapRes.data.choices[0].text
         
-        const body = {
+        const newUrl = await cloudinary.uploader.upload(photo)
+        
+        let body = {
             userId,
+            imgRes: newUrl.secure_url,
             capRes: caption,
-            imgRes: photo,
             ...req.body
         }
+        
         let post = await Post.create(body)
-        res.json(post)
-
+        res.status(201).send(post)
 
         
 
