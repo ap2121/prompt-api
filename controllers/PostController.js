c3444e99a66ad8f36e4dbe1347a091acfea7e173
Arthur Pate <arthurpate42@gmail.com>
2023-03-25 12:06:48 -0700

    updated prompt for image to draw whatever topic user prompts instead of having to direct the ai to draw

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -10,24 +10,20 @@ const configuration = new Configuration({
     organization: OPEN_AI_ORG_KEY
     
 })
-
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-
 const openAi = new OpenAIApi(configuration)
-
 const NewPost = async (req, res) => {
     try {
         const {capPrompt, imgPrompt} = req.body
         const userId = parseInt(req.params.id)
-
         const aiImgRes = await openAi.createImage({
-            prompt: imgPrompt,
+            prompt: `Draw a picture of ${imgPrompt}`,
             n: 1,
-            size: '1024x1024',
+            size: '512x512',
             response_format: 'url'
         })
         const aiCapRes = await openAi.createCompletion({
@@ -35,33 +31,22 @@ const NewPost = async (req, res) => {
             prompt: `Write a very short instagram type caption about ${capPrompt}`,
             temperature: 0.5,
             max_tokens: 150
-
-
-        })
-        
-
+})
         const photo = aiImgRes.data.data[0].url
         const caption = aiCapRes.data.choices[0].text
-        
         const newUrl = await cloudinary.uploader.upload(photo)
-        
         let body = {
             userId,
             imgRes: newUrl.secure_url,
             capRes: caption,
             ...req.body
         }
-        
         let post = await Post.create(body)
         res.status(201).send(post)
-
-        
-
     } catch(error) {
         throw error
     }
 }
-
 module.exports = {
     NewPost
 }
