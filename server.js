b8440a0829c588e637fff3e64d3b1ea42bfecf19
Arthur Pate <arthurpate42@gmail.com>
2023-04-04 09:02:02 -0700

    fixed config to js for deployment

--- server.js
+++ server.js
@@ -5,12 +5,13 @@ const logger = require('morgan')
 const app = express()
 const AppRouter = require('./routes/AppRouter')
 
+
 const PORT = process.env.PORT || 3001
 
 app.use(cors())
 app.use(logger('dev'))
-app.use(express.json({limit: '50mb'}))
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
+
 
 app.get('/', (req, res) => res.json({message: 'Server Works'}))
 app.use('/api', AppRouter)
